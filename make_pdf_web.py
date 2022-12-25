d88e0dd3d4121c1b3ee03f38e92508f76f506520
YasuoKatou <yasuokatou@gmail.com>
2022-12-25 20:29:05 +0900

    請求書のサンプル001作成中

---
+++ make_pdf_web.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/b001')
+def b001():
+    return app.send_static_file('bill_sample001.html')
+
+@app.route('/b001/preview', methods=['POST'])
+def b001_preview():
+    #for key, val in request.form.items():
+    #    print('{}: {}'.format(key, val))
+    return render_template('bill_sample001.html', data=request.form)
+
+if __name__ == '__main__':
+    app.run(port=8891, debug=True)
+
+#[EOF]
